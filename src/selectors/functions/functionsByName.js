ca60cd50709c1d639d65a6e062c96a9bf08c25b1
Martin Middel <martin.middel@liones.nl>
2016-08-22 17:23:42 +0200

    Make to operator faster.

--- src/selectors/functions/functionsByName.js
+++ src/selectors/functions/functionsByName.js
@@ -90,7 +90,11 @@ define([
 			}
 
 			// RangeExpr is inclusive: 1 to 3 will make (1,2,3)
-			return new Sequence(new Array(to - from + 1).fill(0).map(function (_, i) {return new IntegerValue(from + i);}));
+			return new Sequence(
+				Array.apply(null, {length: to - from + 1})
+					.map(function (_, i) {
+						return new IntegerValue(from+i);
+					}));
 		},
 		'concat': function (dynamicContext) {
 			var stringSequences = Array.from(arguments).slice(1);
