4ee251867bcf8d9b319febf202416b73028442e5
Martin Middel <martin.middel@liones.nl>
2018-06-19 17:16:39 +0200

    Fix sorting in path operator
    This fixes #36 and a single QT3 test

--- test/specs/parsing/path/PathSelector.tests.js
+++ test/specs/parsing/path/PathSelector.tests.js
@@ -137,6 +137,10 @@ describe('relative paths', () => {
 		chai.assert.equal(evaluateXPathToString('(/someNode/someChildNode//*)!name()', documentNode.documentElement), 'someGrandChildA1 someGrandChildA2 someGrandChildB1 someGrandChildB2 someGrandChildC1 someGrandChildC2');
 	});
 
+	it('sorts //text() across different levels correctly', () => {
+		chai.assert.equal(evaluateXPathToString('let $dom := <ref id="bib12"><mixed-citation publication-type="journal"><?AuthorQuery id="Q14" queryText="CrossRef reports the DOI should be &quot;10.1175/1520-0477(1986)067&lt;1226:SCOPMM&gt;2.0.CO;2&quot;, not &quot;10.1175/1520-0477(1986)067,1226:SCOPMM.2.0.CO;2&quot;. eXtyles has used the CrossRef-supplied DOI. (Ref. &quot;Wilheit, 1986&quot;)"?><person-group person-group-type="author"><string-name><surname>Wilheit</surname>, <given-names>T. T.</given-names></string-name></person-group>, <year>1986</year>: <article-title>Some comments on passive microwave measurement of rain.</article-title> <source>Bull. Amer. Meteor. Soc.</source>, <volume>67</volume>, <fpage>1226</fpage>–<lpage>1232</lpage>, doi:<pub-id pub-id-type="doi">10.1175/1520-0477(1986)067&lt;1226:SCOPMM&gt;2.0.CO;2</pub-id></mixed-citation></ref> return $dom//text() => string-join("~~")', documentNode, null, null, { language: 'XQuery3.1' }), 'Wilheit~~, ~~T. T.~~, ~~1986~~: ~~Some comments on passive microwave measurement of rain.~~Bull. Amer. Meteor. Soc.~~, ~~67~~, ~~1226~~–~~1232~~, doi:~~10.1175/1520-0477(1986)067<1226:SCOPMM>2.0.CO;2');
+	});
+
 	it('sorts descendant-or-self::/child:: axes correctly', () => {
 		jsonMlMapper.parse([
 			'someNode',
