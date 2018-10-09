92495c5d50d86f5333a575552d7783f8bddbca92
Martin Middel <martin.middel@liones.nl>
2018-10-09 16:43:15 +0200

    [skip ci] Implement letExpressions / forExpressions

--- src/expressions/ForExpression.js
+++ src/expressions/ForExpression.js
@@ -8,31 +8,32 @@ import { DONE_TOKEN } from './util/iterators';
  */
 class ForExpression extends Expression {
 	/**
-	 * @param  {!{varName:{prefix:string, namespaceURI:string, name:string}, expression}}  clause
-	 * @param  {!Expression}                       expression
+	 * @param  {{prefix:string, namespaceURI:string, name: string}}    rangeVariable
+	 * @param  {Expression}  clauseExpression
+	 * @param  {Expression}  returnExpression
 	 */
-	constructor (clause, expression) {
+	constructor (rangeVariable, clauseExpression, returnExpression) {
 		super(
-			new Specificity({}),
-			[clause.expression],
+			clauseExpression.specificity.add(returnExpression.specificity),
+			[clauseExpression, returnExpression],
 			{
 				canBeStaticallyEvaluated: false
 			});
 
-		this._prefix = clause.varName.prefix;
-		this._namespaceURI = clause.varName.namespaceURI;
-		this._localName = clause.varName.name;
+		this._prefix = rangeVariable.prefix;
+		this._namespaceURI = rangeVariable.namespaceURI;
+		this._localName = rangeVariable.name;
 
 		this._variableBindingKey = null;
 
 		/**
 		 * @type {!Expression}
 		 */
-		this._clauseExpression = clause.expression;
+		this._clauseExpression = clauseExpression;
 		/**
 		 * @type {!Expression}
 		 */
-		this._returnExpression = expression;
+		this._returnExpression = returnExpression;
 	}
 
 	performStaticEvaluation (staticContext) {
