aff34e8bf451c46d57f5526bf3167c948402ec9a
Jos Verburg <jos.verburg@liones.nl>
2018-10-17 10:55:00 +0200

    [skip ci] Remove preview from demo page

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,6 @@
 import * as fontoxpath from '../src/index';
 
 const xmlSource = document.getElementById('xmlSource');
-const xmlFile = document.getElementById('xmlFile');
 const log = document.getElementById('log');
 const resultText = document.getElementById('resultText');
 const xpathField = document.getElementById('xpathField');
@@ -11,51 +10,11 @@ xmlSource.innerText = `<xml>
 	<herp>Herp</herp>
 	<derp id="durp">derp</derp>
 	<hurr durr="durrdurrdurr">durrrrrr</hurr>
-</xml>
-	`;
+</xml>`;
 
 const domParser = new DOMParser();
 
 let xmlDoc = domParser.parseFromString(xmlSource.innerText, 'text/xml');
-xmlFile.innerHTML = xmlSource.innerText;
-
-function tryEvaluateToNodes () {
-	let resultNodes;
-	try {
-		resultNodes = fontoxpath.evaluateXPathToNodes(
-			xpathField.value,
-			xmlDoc,
-			null,
-			null,
-			{
-				language: allowXQuery.checked ? fontoxpath.evaluateXPath.XQUERY_3_1_LANGUAGE : fontoxpath.evaluateXPath.XPATH_3_1_LANGUAGE
-			}
-		);
-	}
-	catch (err) {
-		xmlFile.innerText = err.message;
-		return false;
-	}
-
-	log.innerText = '';
-
-	const paths = resultNodes.map(
-		node => {
-			if (!node.ownerDocument ||
-				!node.ownerDocument.contains(node)) {
-				return 'false()';
-			}
-			return fontoxpath.evaluateXPathToString(
-				'if (not(self::element())) then "false()" else ancestor-or-self::*!("child::*[" || count(preceding-sibling::*) + 1 || "]") => reverse() => string-join("/")',
-				node);
-		});
-	const htmlNodes = paths
-		.map(path => fontoxpath.evaluateXPathToFirstNode(path, xmlFile, fontoxpath.domFacade))
-		.filter(n => !!n);
-
-	htmlNodes.forEach(node => node.setAttribute('style', 'border: 1px solid red'));
-	return true;
-}
 
 async function rerunXPath () {
 	// Clear results from previous run
@@ -90,13 +49,10 @@ async function rerunXPath () {
 	}
 
 	resultText.innerText = '[' + raw.map(item => `"${item}"`).join(', ') + ']';
-
-	tryEvaluateToNodes();
 }
 
 xmlSource.oninput = _evt => {
 	xmlDoc = domParser.parseFromString(xmlSource.innerText, 'text/xml');
-	xmlFile.innerHTML = xmlDoc.documentElement.outerHTML;
 
 	if (fontoxpath.evaluateXPathToBoolean('//parseerror', xmlDoc, fontoxpath.domFacade)) {
 		log.innerText = 'Error: invalid XML';
@@ -107,7 +63,6 @@ xmlSource.oninput = _evt => {
 xmlSource.oninput = xpathField.oninput = _evt => {
 	try {
 		xmlDoc = domParser.parseFromString(xmlSource.innerText, 'text/xml');
-		xmlFile.innerHTML = xmlDoc.documentElement.outerHTML;
 
 		rerunXPath();
 	}
