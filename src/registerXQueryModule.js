5414d464f903ee1d33cbbdc8838cb71099a60a59
Jos Verburg <jos.verburg@liones.nl>
2018-10-19 13:45:07 +0200

    [skip ci] Load modules without function declarations

--- src/registerXQueryModule.js
+++ src/registerXQueryModule.js
@@ -49,7 +49,9 @@ export default function registerXQueryModule (moduleString) {
 		});
 
 		loadModuleFile(moduleTargetNamespaceURI, moduleDeclaration);
-
+	}
+	else {
+		loadModuleFile(moduleTargetNamespaceURI, { functionDeclarations: [] });
 	}
 
 	return moduleTargetNamespaceURI;
