1f2e185fd59efac83d920a29dbbffb2cde2e44aa
Martin Middel <martin.middel@liones.nl>
2018-10-24 15:43:58 +0200

    Fix indents in the demo server

--- demo/server.js
+++ demo/server.js
@@ -1,17 +1,14 @@
 const staticAlias = require('node-static');
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
 var distFileServer = new staticAlias.Server('./dist');
 var srcFileServer = new staticAlias.Server('./src');
 var demoFileServer = new staticAlias.Server('./demo');
 
 require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
+	request.addListener('end', function () {
+		//
+		// Serve files!
+		//
 		if (request.url.startsWith('/src')) {
 			request.url = request.url.substring(4) + '.js';
 			srcFileServer.serve(request, response);
@@ -24,5 +21,5 @@ require('http').createServer(function (request, response) {
 
 		console.log(request.url);
 		demoFileServer.serve(request, response);
-    }).resume();
+	}).resume();
 }).listen(8080);
