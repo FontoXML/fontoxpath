74cc0abd74b09e3965236992542d5d42dffbde6b
Jos Verburg <jos.verburg@liones.nl>
2019-01-21 09:39:50 +0100

    [skip ci] Add unit test for reading the same map value multiple times in a function

--- test/specs/parsing/VarRef.tests.ts
+++ test/specs/parsing/VarRef.tests.ts
@@ -2,10 +2,11 @@ import * as chai from 'chai';
 import * as slimdom from 'slimdom';
 
 import {
+	evaluateXPathToArray,
 	evaluateXPathToBoolean,
-	evaluateXPathToString,
+	evaluateXPathToFirstNode,
 	evaluateXPathToNumber,
-	evaluateXPathToFirstNode
+	evaluateXPathToString
 } from 'fontoxpath';
 
 let documentNode;
@@ -14,45 +15,95 @@ beforeEach(() => {
 });
 
 describe('varRef', () => {
-	it('can reference "let" variables more than once',
-		() => chai.assert.equal(
-			evaluateXPathToNumber('let $x := (1,2,3,4,5,6) return count($x) + count(reverse($x)) (: If this resolves with 8, it signals a double iteration of the same sequence :)'),
+	it('can reference "let" variables more than once', () =>
+		chai.assert.equal(
+			evaluateXPathToNumber(
+				'let $x := (1,2,3,4,5,6) return count($x) + count(reverse($x)) (: If this resolves with 8, it signals a double iteration of the same sequence :)'
+			),
 			12
 		));
-	it('can reference "function parameters" variables more than once',
-		() => chai.assert.equal(
-			evaluateXPathToNumber('function ($x as item()*) {count($x) + count(reverse($x))}((1,2,3,4,5,6)) (: If this resolves with 8, it signals a double iteration of the same sequence :)'),
+	it('can reference "function parameters" variables more than once', () =>
+		chai.assert.equal(
+			evaluateXPathToNumber(
+				'function ($x as item()*) {count($x) + count(reverse($x))}((1,2,3,4,5,6)) (: If this resolves with 8, it signals a double iteration of the same sequence :)'
+			),
 			12
 		));
 
 	describe('external variables', () => {
-		it('can reference variables: numerical',
-			() => chai.assert.equal(evaluateXPathToNumber('$x', documentNode, null, { x: 42 }), 42));
-		it('can reference variables: maps',
-			() => chai.assert.equal(evaluateXPathToNumber('$x("a")', documentNode, null, { x: { a: 1 } }), 1));
-		it('can reference variables: nodes',
-			() => chai.assert.equal(evaluateXPathToFirstNode('$x("a")/self::node()', documentNode, null, { x: { a: documentNode } }), documentNode));
+		it('can reference variables: numerical', () =>
+			chai.assert.equal(evaluateXPathToNumber('$x', documentNode, null, { x: 42 }), 42));
+		it('can reference variables: maps', () =>
+			chai.assert.equal(
+				evaluateXPathToNumber('$x("a")', documentNode, null, { x: { a: 1 } }),
+				1
+			));
+		it('can reference variables: nodes', () =>
+			chai.assert.equal(
+				evaluateXPathToFirstNode('$x("a")/self::node()', documentNode, null, {
+					x: { a: documentNode }
+				}),
+				documentNode
+			));
 
-		it('can reference variables: arrays',
-			() => chai.assert.equal(evaluateXPathToString('$x(1)', documentNode, null, { x: ['a', 'b', 'c'] }), 'a'));
-		it('can reference variables: mixed maps and arrays',
-			() => chai.assert.equal(evaluateXPathToNumber('$x(1)("a")', documentNode, null, { x: [{ 'a': 123 }] }), 123));
-		it('can reference variables: maps with null values',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('$x("x") => empty()', documentNode, null, { x: { x: null } })));
-		it('can reference variables: maps with undefined values (undefined means absent)',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('$x("x") => empty()', documentNode, null, { x: { x: undefined } })));
-		it('can reference variables: arrays with null values',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('$x(1) => empty()', documentNode, null, { x: [ null ] })));
-		it('can reference variables: arrays with undefined values (undefined means null)',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('$x(1) => empty()', documentNode, null, { x: [ undefined ] })));
+		it('can reference variables: arrays', () =>
+			chai.assert.equal(
+				evaluateXPathToString('$x(1)', documentNode, null, { x: ['a', 'b', 'c'] }),
+				'a'
+			));
+		it('can reference variables: mixed maps and arrays', () =>
+			chai.assert.equal(
+				evaluateXPathToNumber('$x(1)("a")', documentNode, null, { x: [{ a: 123 }] }),
+				123
+			));
+		it('can reference variables: maps with null values', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean('$x("x") => empty()', documentNode, null, { x: { x: null } })
+			));
+		it('can reference variables: maps with undefined values (undefined means absent)', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean('$x("x") => empty()', documentNode, null, {
+					x: { x: undefined }
+				})
+			));
+		it('can reference variables: arrays with null values', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean('$x(1) => empty()', documentNode, null, { x: [null] })
+			));
+		it('can reference variables: arrays with undefined values (undefined means null)', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean('$x(1) => empty()', documentNode, null, { x: [undefined] })
+			));
 
-		it('can reference variables: ',
-			() => chai.assert.equal(evaluateXPathToNumber('$x(1)("a")', documentNode, null, { x: [{ 'a': 123 }] }), 123));
+		it('can reference variables: ', () =>
+			chai.assert.equal(
+				evaluateXPathToNumber('$x(1)("a")', documentNode, null, { x: [{ a: 123 }] }),
+				123
+			));
 
 		it('does not mutate to the external variables object', () => {
 			const variables = { a: 1 };
 			evaluateXPathToString('()', documentNode, undefined, variables);
-			chai.assert.deepEqual(variables, { a: 1 }, 'The passed variables should not be mutated.');
+			chai.assert.deepEqual(
+				variables,
+				{ a: 1 },
+				'The passed variables should not be mutated.'
+			);
 		});
+
+		it.only('can read a map value twice from within a function', () =>
+			chai.assert.equal(
+				evaluateXPathToArray(
+					`declare function local:func($some-map) {
+						[$some-map("k"), $some-map("k")]
+					};
+					local:func($my-map)`,
+					null,
+					null,
+					{ 'my-map': { k: 'val' } },
+					{ language: 'XQuery3.1' }
+				),
+				['val', 'val']
+			));
 	});
 });
