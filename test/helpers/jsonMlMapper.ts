c0adcaa1404f9aee2d10b3695f6f8ea62a198636
Martin Middel <martin.middel@liones.nl>
2019-04-01 12:22:52 +0200

    Upgrade slimdom to 2.3.0

--- test/helpers/jsonMlMapper.ts
+++ test/helpers/jsonMlMapper.ts
@@ -1,6 +1,16 @@
+import {
+	Node,
+	Document,
+	Text,
+	Comment,
+	ProcessingInstruction,
+	Element,
+	DocumentType
+} from 'slimdom';
+
 // Format used is JsonML (http://www.jsonml.org/)
 
-interface JsonML extends Array<string|object|JsonML> {
+interface JsonML extends Array<string | object | JsonML> {
 	0: string;
 }
 
@@ -10,12 +20,12 @@ interface JsonML extends Array<string|object|JsonML> {
  *
  * JsonML is always expected to be a JavaScript structure. If you have a string of JSON, use JSON.parse first.
  *
- * @param   {Document}  document  The document to use to create nodes
- * @param   {JsonML}    jsonml    The JsonML fragment to parse
+ * @param   document  The document to use to create nodes
+ * @param   jsonml    The JsonML fragment to parse
  *
- * @return  {Node}      The root node of the constructed DOM fragment
+ * @return  The root node of the constructed DOM fragment
  */
-export function parseNode(document: Document, jsonml: JsonML|string): Node {
+export function parseNode(document: Document, jsonml: JsonML | string): Node {
 	if (typeof jsonml === 'string') {
 		return document.createTextNode(jsonml);
 	}
@@ -29,10 +39,12 @@ export function parseNode(document: Document, jsonml: JsonML|string): Node {
 	// Processing instructions are not officially part of the JSONML standard,
 	// we therefore encode them as elements containing a single text node
 	if (/^\?/.test(name)) {
-		const target = name.substring(1),
-			data = jsonml[1] || '';
-		if (jsonml.length > 2 || (typeof data !== 'string')) {
-			throw new TypeError('JsonML processing instruction should contain at most a single string');
+		const target = name.substring(1);
+		const data = jsonml[1] || '';
+		if (jsonml.length > 2 || typeof data !== 'string') {
+			throw new TypeError(
+				'JsonML processing instruction should contain at most a single string'
+			);
 		}
 		return document.createProcessingInstruction(target, data);
 	}
@@ -41,7 +53,7 @@ export function parseNode(document: Document, jsonml: JsonML|string): Node {
 	// we therefore encode them as elements named '!' containing zero or one text node
 	if (name === '!') {
 		const comment = jsonml[1] || '';
-		if (jsonml.length > 2 || (typeof comment !== 'string')) {
+		if (jsonml.length > 2 || typeof comment !== 'string') {
 			throw new TypeError('JsonML comment should contain at most a single string');
 		}
 		return document.createComment(comment);
@@ -56,7 +68,8 @@ export function parseNode(document: Document, jsonml: JsonML|string): Node {
 		const doctype = document.implementation.createDocumentType(
 			jsonml[1] as string,
 			jsonml[2] as string,
-			jsonml[3] as string);
+			jsonml[3] as string
+		);
 		return doctype;
 	}
 
@@ -64,7 +77,7 @@ export function parseNode(document: Document, jsonml: JsonML|string): Node {
 	const element = document.createElement(name);
 	const firstChild = jsonml[1];
 	let firstChildIndex = 1;
-	if ((typeof firstChild === 'object') && !Array.isArray(firstChild)) {
+	if (typeof firstChild === 'object' && !Array.isArray(firstChild)) {
 		for (const attributeName in firstChild) {
 			if (firstChild[attributeName] !== null) {
 				element.setAttribute(attributeName, firstChild[attributeName]);
@@ -90,7 +103,7 @@ export function parseNode(document: Document, jsonml: JsonML|string): Node {
  *
  * @return  The given document, with the parse result appended
  */
-export function parse(jsonml: JsonML|string, document: Document): Document {
+export function parse(jsonml: JsonML | string, document: Document): Document {
 	const root = parseNode(document, jsonml);
 
 	document.appendChild(root);
@@ -112,22 +125,26 @@ export function parse(jsonml: JsonML|string, document: Document): Document {
  *
  * @return  {JsonML}
  */
-export function serialize(node: Node): JsonML|string {
+export function serialize(node: Node): JsonML | string {
 	switch (node.nodeType) {
-		case node.TEXT_NODE:
+		case Node.TEXT_NODE:
 			return (node as Text).nodeValue;
-		case node.COMMENT_NODE:
+		case Node.COMMENT_NODE:
 			return (node as Comment).data ? ['!', (node as Comment).data] : ['!'];
-		case node.PROCESSING_INSTRUCTION_NODE:
-			return (node as ProcessingInstruction).data ?
-				['?' + (node as ProcessingInstruction).target, (node as ProcessingInstruction).data] :
-				['?' + (node as ProcessingInstruction).target];
-		case node.DOCUMENT_TYPE_NODE:
+		case Node.PROCESSING_INSTRUCTION_NODE:
+			return (node as ProcessingInstruction).data
+				? [
+						'?' + (node as ProcessingInstruction).target,
+						(node as ProcessingInstruction).data
+				  ]
+				: ['?' + (node as ProcessingInstruction).target];
+		case Node.DOCUMENT_TYPE_NODE:
 			return [
 				'!DOCTYPE',
 				(node as DocumentType).name,
 				(node as DocumentType).publicId,
-				(node as DocumentType).systemId];
+				(node as DocumentType).systemId
+			];
 		default:
 			// Serialize element
 			const jsonml = [node.nodeName] as JsonML;
@@ -144,7 +161,11 @@ export function serialize(node: Node): JsonML|string {
 			}
 
 			// Serialize child nodes
-			for (let childNode: Node = (node as Element).firstChild; childNode; childNode = childNode.nextSibling) {
+			for (
+				let childNode: Node = (node as Element).firstChild;
+				childNode;
+				childNode = childNode.nextSibling
+			) {
 				jsonml.push(serialize(childNode));
 			}
 
@@ -152,7 +173,7 @@ export function serialize(node: Node): JsonML|string {
 	}
 }
 export default {
-	parse: parse,
-	parseNode: parseNode,
-	serialize: serialize
+	parse,
+	parseNode,
+	serialize
 };
