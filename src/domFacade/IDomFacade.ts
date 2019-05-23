acf40da7ec4cfa28d7a62b1e5ef7830facc8e4d0
Mehmet Coskun <mehmet.coskun@fontoxml.com>
2019-05-23 12:21:04 +0200

    DEV-2386 Lint issue solved

--- src/domFacade/IDomFacade.ts
+++ src/domFacade/IDomFacade.ts
@@ -10,16 +10,16 @@ export default interface IDomFacade {
 	 * Get all attributes of this element.
 	 * The bucket can be used to narrow down which attributes should be retrieved.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the attribute that will be used.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the attribute that will be used.
 	 */
 	getAllAttributes(node: Element, bucket: string | null): Attr[];
 
 	/**
 	 * Get the value of specified attribute of this element.
 	 *
-	 * @param  node
-	 * @param  attributeName
+	 * @param  node -
+	 * @param  attributeName -
 	 */
 	getAttribute(node: Element, attributeName: string): string | null;
 
@@ -27,15 +27,15 @@ export default interface IDomFacade {
 	 * Get all child nodes of this element.
 	 * The bucket can be used to narrow down which child nodes should be retrieved.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the attribute that will be used.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the attribute that will be used.
 	 */
 	getChildNodes(node: Node, bucket: string | null): Node[];
 
 	/**
 	 * Get the data of this element.
 	 *
-	 * @param  node
+	 * @param  node -
 	 */
 	getData(node: Attr | CharacterData): string;
 
@@ -43,8 +43,8 @@ export default interface IDomFacade {
 	 * Get the first child of this element.
 	 * An implementation of IDomFacade is free to interpret the bucket to skip returning nodes that do not match the bucket, or use this information to its advantage.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the attribute that will be used.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the attribute that will be used.
 	 */
 	getFirstChild(node: Node, bucket: string | null): Node | null;
 
@@ -52,8 +52,8 @@ export default interface IDomFacade {
 	 * Get the last child of this element.
 	 * An implementation of IDomFacade is free to interpret the bucket to skip returning nodes that do not match the bucket, or use this information to its advantage.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the attribute that will be used.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the attribute that will be used.
 	 */
 	getLastChild(node: Node, bucket: string | null): Node | null;
 
@@ -61,8 +61,8 @@ export default interface IDomFacade {
 	 * Get the next sibling of this node
 	 * An implementation of IDomFacade is free to interpret the bucket to skip returning nodes that do not match the bucket, or use this information to its advantage.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the nextSibling that is requested.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the nextSibling that is requested.
 	 */
 	getNextSibling(node: Node, bucket: string | null): Node | null;
 
@@ -70,8 +70,8 @@ export default interface IDomFacade {
 	 * Get the parent of this element.
 	 * An implementation of IDomFacade is free to interpret the bucket to skip returning nodes that do not match the bucket, or use this information to its advantage.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the attribute that will be used.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the attribute that will be used.
 	 */
 	getParentNode(node: Node, bucket: string | null): Node | null;
 
@@ -79,8 +79,8 @@ export default interface IDomFacade {
 	 * Get the previous sibling of this element.
 	 * An implementation of IDomFacade is free to interpret the bucket to skip returning nodes that do not match the bucket, or use this information to its advantage.
 	 *
-	 * @param  node
-	 * @param  bucket  The bucket that matches the attribute that will be used.
+	 * @param  node -
+	 * @param  bucket - The bucket that matches the attribute that will be used.
 	 */
 	getPreviousSibling(node: Node, bucket: string | null): Node | null;
 }
