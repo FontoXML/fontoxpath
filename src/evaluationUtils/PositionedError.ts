1a8e0829a0714a5a86d0f8436f271283f7c4838d
Jari <jarivanmenxel@gmail.com>
2019-08-19 15:34:53 +0200

    Improve readability

--- src/evaluationUtils/PositionedError.ts
+++ src/evaluationUtils/PositionedError.ts
@@ -1,6 +1,9 @@
 import { SourceRange } from '../expressions/debug/StackTraceGenerator';
+
 export class PositionedError extends Error {
+	
 	public readonly position: SourceRange;
+
 	constructor(message: string, position: SourceRange) {
 		super(message);
 		this['position'] = {
