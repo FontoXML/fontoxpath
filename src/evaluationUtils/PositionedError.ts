82942eb8995efca604839099936bdc1d103e0ad5
Jari <jarivanmenxel@gmail.com>
2019-08-19 15:46:00 +0200

    Change formatting

--- src/evaluationUtils/PositionedError.ts
+++ src/evaluationUtils/PositionedError.ts
@@ -1,7 +1,6 @@
 import { SourceRange } from '../expressions/debug/StackTraceGenerator';
 
 export class PositionedError extends Error {
-	
 	public readonly position: SourceRange;
 
 	constructor(message: string, position: SourceRange) {
