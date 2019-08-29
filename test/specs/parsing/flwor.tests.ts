793ea3658b3fa671f71e8d925d59b850c72760b7
David <d.sanchez@bi4group.com>
2019-08-29 15:24:11 +0200

    Beutify flwor test and add test to the blacklist

--- test/specs/parsing/flwor.tests.ts
+++ test/specs/parsing/flwor.tests.ts
@@ -1,34 +1,40 @@
 import * as chai from 'chai';
 import * as slimdom from 'slimdom';
 
-import { evaluateXPath, evaluateXPathToNumber, evaluateXPathToString } from 'fontoxpath';
+import { evaluateXPath, evaluateXPathToString } from 'fontoxpath';
 
 let documentNode;
 beforeEach(() => {
 	documentNode = new slimdom.Document();
 });
 
-describe('FLWOR', () => {
-	it('runs basic flwor expression', () => chai.assert.equal(
-		evaluateXPathToString(
-			`for $i in (0,1,2)
-			let $e := 'Hello'
-			return $e`,
-			null,
-			null,
-			null,
-			{ debug: true, language: evaluateXPath.XQUERY_3_1_LANGUAGE }
-		), 'Hello Hello Hello'));
+describe.only('FLWOR', () => {
+	it('runs basic flwor expression', () =>
+		chai.assert.equal(
+			evaluateXPathToString(
+				`for $i in (0,1,2)
+				let $e := 'Hello'
+				return $e`,
+				null,
+				null,
+				null,
+				{ debug: true, language: evaluateXPath.XQUERY_3_1_LANGUAGE }
+			),
+			'Hello Hello Hello'
+		));
 
-	it('runs flwor with where', () => chai.assert.equal(
-		evaluateXPathToString(
-			`for $i in (0,1,2)
+	it('runs flwor with where', () =>
+		chai.assert.equal(
+			evaluateXPathToString(
+				`for $i in (0,1,2)
 			where $i = 1
 			let $e := 'Hello'
 			return $e`,
-			null,
-			null,
-			null,
-			{ debug: true, language: evaluateXPath.XQUERY_3_1_LANGUAGE }
-		), "Hello"));
+				null,
+				null,
+				null,
+				{ debug: true, language: evaluateXPath.XQUERY_3_1_LANGUAGE }
+			),
+			'Hello'
+		));
 });
