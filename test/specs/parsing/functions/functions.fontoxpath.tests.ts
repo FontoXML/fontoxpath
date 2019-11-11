d272000317fd58511364598d58ef8be3afa0104b
Martin Middel <martin.middel@liones.nl>
2019-11-11 13:58:55 +0100

    Make fontoxpath:evaluate also evaluate XQuery scripts

--- test/specs/parsing/functions/functions.fontoxpath.tests.ts
+++ test/specs/parsing/functions/functions.fontoxpath.tests.ts
@@ -1,11 +1,11 @@
 import * as chai from 'chai';
-import * as slimdom from 'slimdom';
-
 import {
 	domFacade,
 	evaluateXPathToBoolean,
+	evaluateXPathToNumber,
 	evaluateXPathToString
 } from 'fontoxpath';
+import * as slimdom from 'slimdom';
 
 let documentNode;
 beforeEach(() => {
@@ -17,25 +17,52 @@ describe('extension functions', () => {
 		it('returns the version', () => {
 			try {
 				chai.assert.equal(evaluateXPathToString('fontoxpath:version()'), 'devbuild');
-			}
-			catch (_error) {
-				chai.assert.match(
-					evaluateXPathToString('fontoxpath:version()'),
-					/\d+\.\d+\.\d+/);
+			} catch (_error) {
+				chai.assert.match(evaluateXPathToString('fontoxpath:version()'), /\d+\.\d+\.\d+/);
 			}
 		});
 	});
 	describe('fontoxpath:evaluate()', () => {
-		it('can run inline functions',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('fontoxpath:evaluate("true()", map{})', documentNode, domFacade)));
-		it('can iterate over the same sequence, inside the evaluate call',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('fontoxpath:evaluate("count($x) + count(reverse($x))", map{"x": (1,2,3,4,5)})', documentNode, domFacade)));
-		it('can run inline inline functions',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('fontoxpath:evaluate("fontoxpath:evaluate(""true()"", map{})", map{})', documentNode, domFacade)));
-		it('can run inside inline functions',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('function() {fontoxpath:evaluate("true()", map{})}()', documentNode, domFacade)));
-		it('accepts parameters',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('fontoxpath:evaluate("$a", map{"a":true()})', documentNode, domFacade)));
+		it('can run inline functions', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean(
+					'fontoxpath:evaluate("true()", map{})',
+					documentNode,
+					domFacade
+				)
+			));
+		it('can iterate over the same sequence, inside the evaluate call', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean(
+					'fontoxpath:evaluate("count($x) + count(reverse($x))", map{"x": (1,2,3,4,5)})',
+					documentNode,
+					domFacade
+				)
+			));
+		it('can run inline inline functions', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean(
+					'fontoxpath:evaluate("fontoxpath:evaluate(""true()"", map{})", map{})',
+					documentNode,
+					domFacade
+				)
+			));
+		it('can run inside inline functions', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean(
+					'function() {fontoxpath:evaluate("true()", map{})}()',
+					documentNode,
+					domFacade
+				)
+			));
+		it('accepts parameters', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean(
+					'fontoxpath:evaluate("$a", map{"a":true()})',
+					documentNode,
+					domFacade
+				)
+			));
 		it('retains namespaces in scope', () => {
 			chai.assert.isTrue(
 				evaluateXPathToBoolean(
@@ -43,10 +70,27 @@ describe('extension functions', () => {
 					documentNode,
 					domFacade,
 					{},
-					{ namespaceResolver: prefix => ({ fun: 'http://www.w3.org/2005/xpath-functions' }[prefix]) }));
+					{
+						namespaceResolver: prefix =>
+							({ fun: 'http://www.w3.org/2005/xpath-functions' }[prefix])
+					}
+				)
+			);
 		});
-		it('accepts "." as contextItem',
-			() => chai.assert.isTrue(evaluateXPathToBoolean('fontoxpath:evaluate(".", map{".":true()})', documentNode, domFacade)));
-
+		it('accepts "." as contextItem', () =>
+			chai.assert.isTrue(
+				evaluateXPathToBoolean(
+					'fontoxpath:evaluate(".", map{".":true()})',
+					documentNode,
+					domFacade
+				)
+			));
+		it('also allows XQuery to be evaluated', () =>
+			chai.assert.equal(
+				evaluateXPathToNumber(
+					'fontoxpath:evaluate("declare default function namespace ""http://www.w3.org/2005/xpath-functions/math""; exp10(3)", map{})'
+				),
+				1000
+			));
 	});
 });
