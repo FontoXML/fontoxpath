35a8ddd43337a268853ad4bcae739cbaee2e8bb1
Martin Middel <martin.middel@liones.nl>
2020-05-18 12:24:17 +0200

    Ensure future bucket correctness by checking them in DomFacade
    also, this imporves performance by a bit

--- src/domFacade/ExternalDomFacade.ts
+++ src/domFacade/ExternalDomFacade.ts
@@ -1,13 +1,18 @@
 import { Attr, CharacterData, Node } from '../types/Types';
 import { NODE_TYPES } from './ConcreteNode';
 import IDomFacade from './IDomFacade';
+import { getBucketsForNode } from '../getBuckets';
 
 export default class ExternalDomFacade implements IDomFacade {
-	public ['getAllAttributes'](node: Node): Attr[] {
+	public ['getAllAttributes'](node: Node, bucket: string | null = null): Attr[] {
 		if (node.nodeType !== NODE_TYPES.ELEMENT_NODE) {
 			return [];
 		}
-		return Array.from(node['attributes']);
+		const attrs = Array.from(node['attributes']) as Attr[];
+		if (bucket === null) {
+			return attrs;
+		}
+		return attrs.filter((attr) => getBucketsForNode(attr).includes(bucket));
 	}
 	public ['getAttribute'](node: Node, attributeName: string): string {
 		if (node.nodeType !== NODE_TYPES.ELEMENT_NODE) {
@@ -15,28 +20,75 @@ export default class ExternalDomFacade implements IDomFacade {
 		}
 		return node['getAttribute'](attributeName);
 	}
-	public ['getChildNodes'](node: Node): Node[] {
-		return Array.from(node['childNodes']);
+	public ['getChildNodes'](node: Node, bucket: string | null = null): Node[] {
+		const childNodes = Array.from(node['childNodes'] as Node[]);
+
+		if (bucket === null) {
+			return childNodes;
+		}
+
+		return childNodes.filter((child) => getBucketsForNode(child).includes(bucket));
 	}
 	public ['getData'](node: Attr | CharacterData): string {
 		return node['nodeType'] === NODE_TYPES.ATTRIBUTE_NODE ? node['value'] : node['data'];
 	}
-	public ['getFirstChild'](node: Node): Node {
-		return node['firstChild'] as Node;
-	}
-	public ['getLastChild'](node: Node): Node {
-		return node['lastChild'] as Node;
-	}
-	public ['getNextSibling'](node: Node): Node {
-		return node['nextSibling'];
-	}
-	public ['getParentNode'](node: Node): Node {
-		if (node['nodeType'] === NODE_TYPES.ATTRIBUTE_NODE) {
-			return node['ownerElement'];
+	public ['getFirstChild'](node: Node, bucket: string | null = null): Node | null {
+		for (let child = node['firstChild'] as Node; child; child = child['nextSibling'] as Node) {
+			if (bucket === null || getBucketsForNode(child).includes(bucket)) {
+				return child;
+			}
 		}
-		return node['parentNode'] as Node;
+
+		return null;
 	}
-	public ['getPreviousSibling'](node: Node): Node {
-		return node['previousSibling'];
+	public ['getLastChild'](node: Node, bucket: string | null = null): Node | null {
+		for (
+			let child = node['lastChild'] as Node;
+			child;
+			child = child['previousSibling'] as Node
+		) {
+			if (bucket === null || getBucketsForNode(child).includes(bucket)) {
+				return child;
+			}
+		}
+
+		return null;
+	}
+	public ['getNextSibling'](node: Node, bucket: string | null = null): Node | null {
+		for (
+			let sibling = node['nextSibling'] as Node;
+			sibling;
+			sibling = sibling['nextSibling'] as Node
+		) {
+			if (bucket === null || getBucketsForNode(sibling).includes(bucket)) {
+				return sibling;
+			}
+		}
+
+		return null;
+	}
+	public ['getParentNode'](node: Node, bucket: string | null = null): Node {
+		const parentNode =
+			node['nodeType'] === NODE_TYPES.ATTRIBUTE_NODE
+				? node['ownerElement']
+				: (node['parentNode'] as Node);
+
+		if (bucket === null || getBucketsForNode(parentNode).includes(bucket)) {
+			return parentNode;
+		}
+		return null;
+	}
+	public ['getPreviousSibling'](node: Node, bucket: string | null = null): Node | null {
+		for (
+			let sibling = node['previousSibling'] as Node;
+			sibling;
+			sibling = sibling['previousSibling'] as Node
+		) {
+			if (bucket === null || getBucketsForNode(sibling).includes(bucket)) {
+				return sibling;
+			}
+		}
+
+		return null;
 	}
 }
