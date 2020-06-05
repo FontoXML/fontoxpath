921cb5bcf56e3cf3b1f09f5412ffd3ad42d64b69
Martin Middel <martin.middel@liones.nl>
2020-06-05 15:00:17 +0200

    Extend perf benchmark for sorting by sorting many many items

--- performance/union.benchmark.ts
+++ performance/union.benchmark.ts
@@ -6,22 +6,20 @@ import { sync } from 'slimdom-sax-parser';
 let document: Document;
 
 function setup() {
-    document = sync(`
+	document = sync(`
     <xml>
         <title>xpath.playground.fontoxml.com</title>
         <summary>This is a learning tool for XML, XPath and XQuery.</summary>
         <tips>
-            <tip id='edit'>You can edit everything on the left</tip>
-            <tip id='examples'>You can access more examples from a menu in the top right</tip>
-            <tip id='permalink'>Another button there lets you share your test using an URL</tip>
+		${new Array(1000).fill('<tip>with some content</tip>')}
         </tips>
-    </xml>`)
+    </xml>`);
 }
 
 benchmarkRunner.addBenchmark(
 	'union',
 	() => {
-		evaluateXPath('/xml/title | /xml/tips/tip[id=\'permalink\'] | /xml/summary', document, domFacade);
+		evaluateXPath('//tip | reverse(//tip)', document, domFacade);
 	},
 	setup
 );
