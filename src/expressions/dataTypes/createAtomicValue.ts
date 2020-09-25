cf8be2325929cf7138c32ebbc0c50ac7d44c977c
Martin Middel <martin.middel@liones.nl>
2020-09-25 11:32:26 +0200

    Remove a bunch of dead code
    We do not allow loading schemata or defining custom types, so a lot of the typing infra is never used

--- src/expressions/dataTypes/createAtomicValue.ts
+++ src/expressions/dataTypes/createAtomicValue.ts
@@ -8,18 +8,6 @@ export default function createAtomicValue(value: any, type: ValueType): AtomicVa
 		throw new Error('Unknown type');
 	}
 
-	// if (type === 'xs:boolean' || type === 'xs:string' || type === 'xs:untypedAtomic' || type === 'xs:integer') {
-	// 	let cache = valueCacheByValueByType[type];
-
-	// 	if (!cache) {
-	// 		cache = valueCacheByValueByType[type] = Object.create(null);
-	// 	}
-	// 	let existingValue = cache[value];
-	// 	if (!existingValue) {
-	// 		existingValue = { type: type, value: value };
-	// 	}
-	// 	return existingValue;
-	// }
 	return {
 		type,
 		value,
--- src/expressions/dataTypes/facets/facetsByDataTypeName.ts
+++ src/expressions/dataTypes/facets/facetsByDataTypeName.ts
@@ -1,56 +1,4 @@
-import dateTimeComparator from './comparators/dateTimeComparator';
 import decimalComparator from './comparators/decimalComparator';
-import doubleComparator from './comparators/doubleComparator';
-import durationComparator from './comparators/durationComparator';
-
-// enumeration
-function validateEnumeration(value, enumeration) {
-	return enumeration.includes(value);
-}
-function validateEnumerationList(value, enumeration) {
-	return value.split(' ').every(function (val) {
-		return enumeration.includes(val);
-	});
-}
-
-// length, minLength, maxLength
-function getStringLength(value) {
-	// String is measured in code points
-	return value.length;
-}
-function getHexBinaryLength(value) {
-	return value.length / 2;
-}
-function getBase64Length(value) {
-	return Math.floor((value.replace(/[\u0009\u000A\u000D\u0020=]/g, '').length * 3) / 4);
-}
-function getListLength(value) {
-	return value.split(' ').length;
-}
-function createLengthFacet(getLengthFunction) {
-	return function validateLength(value, length) {
-		return getLengthFunction(value) === length;
-	};
-}
-function createMinLengthFacet(getLengthFunction) {
-	return function validateMinLength(value, minLength) {
-		return getLengthFunction(value) >= minLength;
-	};
-}
-function createMaxLengthFacet(getLengthFunction) {
-	return function validateMaxLength(value, maxLength) {
-		return getLengthFunction(value) <= maxLength;
-	};
-}
-
-// totalDigits
-function validateTotalDigits(value, totalDigits) {
-	const regex = /^[-+]?0*([1-9]\d*)?(?:\.((?:\d*[1-9])*)0*)?$/,
-		match = regex.exec(value),
-		total = (match[1] ? match[1].length : 0) + (match[2] ? match[2].length : 0);
-
-	return total <= totalDigits;
-}
 
 // fractionDigits
 function validateFractionDigits(value, fractionDigits) {
@@ -103,138 +51,47 @@ function validateExplicitTimeZone(value, option) {
 }
 
 export default {
-	'xs:string': {
-		length: createLengthFacet(getStringLength),
-		minLength: createMinLengthFacet(getStringLength),
-		maxLength: createMaxLengthFacet(getStringLength),
-		enumeration: validateEnumeration,
-	},
+	'xs:string': {},
 	'xs:boolean': {},
-	'xs:float': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(doubleComparator),
-		maxExclusive: createMaxExclusiveFacet(doubleComparator),
-		minInclusive: createMinInclusiveFacet(doubleComparator),
-		minExclusive: createMinExclusiveFacet(doubleComparator),
-	},
-	'xs:double': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(doubleComparator),
-		maxExclusive: createMaxExclusiveFacet(doubleComparator),
-		minInclusive: createMinInclusiveFacet(doubleComparator),
-		minExclusive: createMinExclusiveFacet(doubleComparator),
-	},
+	'xs:float': {},
+	'xs:double': {},
 	'xs:decimal': {
-		totalDigits: validateTotalDigits,
 		fractionDigits: validateFractionDigits,
-		enumeration: validateEnumeration,
 		maxInclusive: createMaxInclusiveFacet(decimalComparator),
 		maxExclusive: createMaxExclusiveFacet(decimalComparator),
 		minInclusive: createMinInclusiveFacet(decimalComparator),
 		minExclusive: createMinExclusiveFacet(decimalComparator),
 	},
-	'xs:duration': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(durationComparator),
-		maxExclusive: createMaxExclusiveFacet(durationComparator),
-		minInclusive: createMinInclusiveFacet(durationComparator),
-		minExclusive: createMinExclusiveFacet(durationComparator),
-	},
+	'xs:duration': {},
 	'xs:dateTime': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:time': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:date': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:gYearMonth': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:gYear': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:gMonthDay': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:gDay': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
 	'xs:gMonth': {
-		enumeration: validateEnumeration,
-		maxInclusive: createMaxInclusiveFacet(dateTimeComparator),
-		maxExclusive: createMaxExclusiveFacet(dateTimeComparator),
-		minInclusive: createMinInclusiveFacet(dateTimeComparator),
-		minExclusive: createMinExclusiveFacet(dateTimeComparator),
 		explicitTimezone: validateExplicitTimeZone,
 	},
-	'xs:hexBinary': {
-		length: createLengthFacet(getHexBinaryLength),
-		minLength: createMinLengthFacet(getHexBinaryLength),
-		maxLength: createMaxLengthFacet(getHexBinaryLength),
-		enumeration: validateEnumeration,
-	},
-	'xs:base64Binary': {
-		length: createLengthFacet(getBase64Length),
-		minLength: createMinLengthFacet(getBase64Length),
-		maxLength: createMaxLengthFacet(getBase64Length),
-		enumeration: validateEnumeration,
-	},
-	'xs:anyURI': {
-		length: createLengthFacet(getStringLength),
-		minLength: createMinLengthFacet(getStringLength),
-		maxLength: createMaxLengthFacet(getStringLength),
-		enumeration: validateEnumeration,
-	},
-	'xs:QName': {
-		enumeration: validateEnumeration,
-	},
-	'xs:NOTATION': {
-		enumeration: validateEnumeration,
-	},
-	list: {
-		length: createLengthFacet(getListLength),
-		minLength: createMinLengthFacet(getListLength),
-		maxLength: createMaxLengthFacet(getListLength),
-		enumeration: validateEnumerationList,
-	},
-	union: {
-		enumeration: validateEnumeration,
-	},
+	'xs:hexBinary': {},
+	'xs:base64Binary': {},
+	'xs:anyURI': {},
+	'xs:QName': {},
+	'xs:NOTATION': {},
+	list: {},
+	union: {},
 };
