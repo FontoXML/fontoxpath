4e1063e852c3bd10a8a59b8e9ed89131a261001e
Martin Middel <martin.middel@fontoxml.com>
2021-05-03 19:45:25 +0200

    Remove dead occurence of asyncness

--- src/expressions/util/iterators.ts
+++ src/expressions/util/iterators.ts
@@ -1,11 +1,9 @@
 export class IterationResult<T> {
 	public done: boolean;
-	public promise: Promise<void> | undefined;
 	public value: T | undefined | null;
-	constructor(done: boolean, value: T | undefined, promise: Promise<void> | undefined) {
+	constructor(done: boolean, value: T | undefined) {
 		this.done = done;
 		this.value = value;
-		this.promise = promise;
 	}
 }
 
@@ -14,9 +12,9 @@ export enum IterationHint {
 	SKIP_DESCENDANTS = 1 << 0,
 }
 
-export const DONE_TOKEN = new IterationResult(true, undefined, undefined);
+export const DONE_TOKEN = new IterationResult(true, undefined);
 export function ready<T>(value: T) {
-	return new IterationResult(false, value, undefined);
+	return new IterationResult(false, value);
 }
 
 export interface IIterator<T> {
