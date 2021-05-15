80510a3be0865d7bbfb189ab8cc399246b08d296
devatwork <b.willems@premotion.nl>
2021-05-15 16:40:00 +0200

    Implement multi-core fuzzer
    Refactor the fuzzer implementation into the engine and the
    corpus_based_fuzzer components. This allowes multiple fuzzers
    to co-exist side-by-side.
    
    The engine is overhauled to support running on multiple core in
    parallel. Big shoutout to @mvanbutselaar for his help with the
    implementation. The output of the engine is colorized for improved
    readability.

---
+++ fuzzers/engine.ts
@@ -0,0 +1,189 @@
+import readline from 'readline';
+import { evaluateXPath } from 'fontoxpath';
+const { Worker: ThreadWorker, isMainThread, parentPort, workerData } = require('worker_threads');
+
+/**
+ * Interface for fuzzer which are run by the {@link Engine}.
+ */
+export interface Fuzzer {
+	isExpectedError(error: Error): boolean;
+	globalInit(): void;
+	prepareCase(): FuzzCase;
+}
+
+/**
+ * A single executable fuzz case.
+ */
+export class FuzzCase {
+	contextItem?: any | null;
+	selector: string;
+	language: string;
+
+	constructor(selector: string, language: string, contextItem?: any | null) {
+		this.selector = selector;
+		this.language = language;
+		this.contextItem = contextItem;
+	}
+
+	run(): void {
+		// Execute the expression
+		evaluateXPath(this.selector, this.contextItem, null, null, null, {
+			disableCache: true,
+			language: this.language
+		});
+	}
+}
+
+enum WorkerMessageTypes {
+	Online = 'online',
+	Progress = 'progress',
+	Crash = 'crash',
+}
+
+/**
+ * Multi-threaded fuzzing engine.
+ */
+export default class Engine<TFuzzer extends Fuzzer> {
+	run(fuzzer: TFuzzer, filename: string): void {
+		// Main entry point of the program
+		if (!isMainThread) {
+			this.run_worker(fuzzer)
+		} else {
+			this.run_main(filename);
+		}
+	}
+
+	private run_worker(fuzzer: TFuzzer): void {
+		// Init the fuzzer
+		fuzzer.globalInit();
+
+		// Report worker as online
+		const tid = workerData.tid;
+		parentPort.postMessage({
+			type: WorkerMessageTypes.Online,
+			tid
+		});
+
+		let deltaCases = 0;
+		const uniqueStacks = new Set();
+		while (true) {
+			// Report progress
+			if (deltaCases === 1000) {
+				parentPort.postMessage({
+					type: WorkerMessageTypes.Progress,
+					totalCases: deltaCases,
+					tid
+				});
+				deltaCases = 0;
+			}
+			deltaCases += 1;
+
+			// Init case
+			const fuzzCase = fuzzer.prepareCase();
+
+			// Execute the case
+			try {
+				fuzzCase.run();
+			} catch (error) {
+				// Test if this is an expected error
+				if (fuzzer.isExpectedError(error)) {
+					continue;
+				}
+
+				// Not interested in duplicate stack traces
+				if (uniqueStacks.has(error.stack)) {
+					continue;
+				}
+				uniqueStacks.add(error.stack);
+
+				// Report the error
+				parentPort.postMessage({
+					type: WorkerMessageTypes.Crash,
+					selector: fuzzCase.selector,
+					language: fuzzCase.language,
+					stack: error.stack,
+					tid
+				});
+			}
+		}
+	}
+
+	private run_main(filename: string): void {
+		let workersOnline = 0;
+		let totalCases = 0;
+		const uniqueStacks = new Set();
+
+		// Start all the workers
+		const os = require('os');
+		const numOfCpus = os.cpus().length;
+		process.stdout.write(`Main thread, launching ${numOfCpus} workers\n`);
+		for (let tid of Array(numOfCpus).keys()) {
+			let worker = new ThreadWorker(
+				`
+				require('tsconfig-paths/register');
+				require('ts-node/register');
+				require(require('worker_threads').workerData.runThisFileInTheWorker);
+				`,
+				{
+					env: {
+						TS_NODE_PROJECT: './fuzzers/tsconfig.json'
+					},
+					eval: true,
+					workerData: {
+						runThisFileInTheWorker: filename,
+						tid: tid
+					}
+				}
+			);
+			worker.on('message', msg => {
+				switch (msg.type) {
+					case WorkerMessageTypes.Online: {
+						workersOnline += 1;
+						break;
+					}
+					case WorkerMessageTypes.Progress: {
+						totalCases += msg.totalCases
+						break;
+					}
+					case WorkerMessageTypes.Crash: {
+						// Not interested in duplicate stack traces
+						// Must do this again because workers do not coordinate
+						if (uniqueStacks.has(msg.stack)) {
+							return;
+						}
+						uniqueStacks.add(msg.stack);
+
+						// Print the error
+						process.stdout.write(
+							`\n\n!!! \x1b[31mCrash found\x1b[0m !!!\nSelector: ${msg.selector}\nLanguage: ${msg.language}\n${msg.stack}\n\n`
+						);
+						break;
+					}
+				}
+			});
+			worker.on('error', err => {
+				process.stderr.write(`Unexpected error of ${tid} with error ${err}\n`);
+			});
+			worker.on('exit', code => {
+				process.stderr.write(`Unexpected exit of ${tid} with exit code ${code}\n`);
+			});
+		}
+
+		// Print stats
+		let startTime = process.hrtime();
+		setInterval(() => {
+			// Only start the timer when at least one worker is online
+			if (workersOnline === 0) {
+				startTime = process.hrtime();
+			}
+
+			const elapsed = process.hrtime(startTime);
+			const fcps = (totalCases / elapsed[0]).toFixed(2);
+			readline.clearLine(process.stdout, 0);
+			readline.cursorTo(process.stdout, 0, null);
+			process.stdout.write(
+				`\x1b[0m\t[Workers: \x1b[32m${workersOnline}\x1b[0m] [Total cases: \x1b[32m${totalCases}\x1b[0m] [fcps: \x1b[32m${fcps}\x1b[0m] [Unique crashes: \x1b[31m${uniqueStacks.size}\x1b[0m]`
+			);
+		}, 1000);
+	}
+}
---
+++ fuzzers/mutators.ts
@@ -0,0 +1,84 @@
+import { evaluateXPath } from 'fontoxpath';
+
+/**
+ * Generates a ranumger number between 0 and {@link upper}.
+ * 
+ * @param upper - The upper bound of the random number.
+ * @returns The generated random nummer.
+ */
+export function rand(upper: number): number {
+	return Math.floor(Math.random() * upper);
+}
+
+/**
+ * Pick a random language XPath, XQuery, XQuery UF.
+ *
+ * @returns The randomly selected language.
+ */
+export function randomLanguage(): string {
+	const idx = rand(3);
+	switch (idx) {
+		case 0:
+			return evaluateXPath.XPATH_3_1_LANGUAGE;
+		case 1:
+			return evaluateXPath.XQUERY_3_1_LANGUAGE;
+		case 2:
+			return evaluateXPath.XQUERY_UPDATE_3_1_LANGUAGE;
+		default:
+			throw new Error('Out of bounds');
+	}
+}
+
+const STRING_POOL =
+	'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-[]{}()<>,./?:;"\'|\\!@#$%^&*+=';
+function randomString(len: number): string {
+	let str = '';
+	for (let ii = 0; ii < len; ii++) {
+		str += STRING_POOL.charAt(rand(STRING_POOL.length));
+	}
+	return str;
+}
+
+/**
+ * Mutate a random number of characters.
+ * 
+ * @param input - The input string which to mutate.
+ * @returns Returns the mutated string.
+ */
+export function mutateCharactersInPlace(input: string): string {
+	for (let ii = 0; ii < rand(4) + 1; ii++) {
+		const idx = rand(input.length);
+		const replacement = randomString(1);
+		input = input.substr(0, idx) + replacement + input.substr(idx + replacement.length);
+	}
+	return input;
+}
+
+/**
+ * Mutate the intput string.
+ * 
+ * @param input - The input string which to mutate.
+ * @returns Returns the mutated string.
+ */
+export function mutateString(input: string): string {
+	let arr = input.split('');
+	// 1 in 20 chance to reverse the input
+	if (rand(20) === 0) {
+		arr = arr.reverse();
+	}
+
+	// 1 in 4 chance to delete random range of chars from the input
+	if (rand(4) === 0) {
+		arr.splice(rand(arr.length), rand(arr.length));
+	}
+
+	// 1 in 4 chance to insert random range of chars
+	if (rand(4) === 0) {
+		const insertions = randomString(rand(arr.length)).split('');
+		const args: any[] = [rand(arr.length), 0];
+		arr.splice.apply(arr, args.concat(insertions));
+	}
+
+	input = arr.join('');
+	return input;
+}
