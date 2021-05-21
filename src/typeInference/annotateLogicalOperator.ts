4a303f49120d7dacaca66e9ee08c30c4264a8480
Yuanze Xiong <Y.Xiong-3@student.tudelft.nl>
2021-05-21 18:51:12 +0200

    Added docs to the and or operator

--- src/typeInference/annotateLogicalOperator.ts
+++ src/typeInference/annotateLogicalOperator.ts
@@ -1,6 +1,11 @@
 import { SequenceMultiplicity, SequenceType, ValueType } from '../expressions/dataTypes/Value';
 import astHelper, { IAST } from '../parsing/astHelper';
 
+/**
+ * Switch cases that take care of the operators under the logical operator category.
+ *
+ * @param ast The AST to annotate.
+ */
 export function annotateLogicalOperator(ast: IAST): SequenceType {
 	switch (ast[0]) {
 		case 'andOp':
@@ -10,6 +15,12 @@ export function annotateLogicalOperator(ast: IAST): SequenceType {
 	}
 }
 
+/**
+ * Inserts a boolean type to the AST, as or operator returns boolean type.
+ *
+ * @param ast the AST to be annotated.
+ * @returns `SequenceType` of type boolean and multiplicity of `Exactly_ONE`.
+ */
 function annotateOrOperator(ast: IAST): SequenceType {
 	const seqType = {
 		type: ValueType.XSBOOLEAN,
@@ -21,6 +32,12 @@ function annotateOrOperator(ast: IAST): SequenceType {
 	return seqType;
 }
 
+/**
+ * Inserts a boolean type to the AST, as and operator returns boolean type.
+ *
+ * @param ast the AST to be annotated.
+ * @returns `SequenceType` of type boolean and multiplicity of `Exactly_ONE`.
+ */
 function annotateAndOperator(ast: IAST): SequenceType {
 	const seqType = {
 		type: ValueType.XSBOOLEAN,
