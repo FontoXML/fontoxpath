a27608f21adf6b10b2e42b2752a6b3ca55550017
Marijn Timmerije <M.Timmerije@student.tudelft.nl>
2021-05-26 16:47:37 +0200

    Implemented named function reference annotation

--- src/typeInference/annotateNamedFunctionRef.ts
+++ src/typeInference/annotateNamedFunctionRef.ts
@@ -1,4 +1,4 @@
-import { SequenceType } from '../expressions/dataTypes/Value';
+import { SequenceType, ValueType, SequenceMultiplicity } from '../expressions/dataTypes/Value';
 import StaticContext from '../expressions/StaticContext';
 import astHelper, { IAST } from '../parsing/astHelper';
 
@@ -14,5 +14,38 @@ export function annotateNamedFunctionRef(
 	ast: IAST,
 	staticContext: StaticContext
 ): SequenceType | undefined {
-	return undefined;
+	// Can't find info about the function without the context.
+	if (!staticContext) return undefined;
+
+	// Get qualified function name
+	const functionQName = astHelper.getQName(astHelper.getFirstChild(ast, 'functionName'));
+
+	// Spice the components up
+	let localName = functionQName[0];
+	let namespaceURI = functionQName[1];
+	const prefix = functionQName[2];
+
+	const arity = astHelper.getFirstChild(ast, 'integerConstantExpr')[1][1];
+
+	// If there is no namespace URI, resolve the function name
+	if (!namespaceURI) {
+		const functionName = staticContext.resolveFunctionName({ localName, prefix }, arity);
+
+		if (!functionName) {
+			return undefined;
+		}
+
+		localName = functionName.localName;
+		namespaceURI = functionName.namespaceURI;
+	}
+
+	// With all components there, look up the function properties
+	const functionProperties = staticContext.lookupFunction(namespaceURI, localName, arity) || null;
+
+	// If there are no function properties, there is no type inference
+	if (!functionProperties) return undefined;
+
+	//Insert the type info into the AST and return
+	astHelper.insertAttribute(ast, 'type', functionProperties.returnType);
+	return functionProperties.returnType;
 }
