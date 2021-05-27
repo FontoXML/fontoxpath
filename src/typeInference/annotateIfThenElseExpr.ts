8af14c18b06a107c6597f15892988e2f2de45061
Yuanze Xiong <Y.Xiong-3@student.tudelft.nl>
2021-05-27 12:10:35 +0200

    Added documentation to the if then else expr

--- src/typeInference/annotateIfThenElseExpr.ts
+++ src/typeInference/annotateIfThenElseExpr.ts
@@ -1,6 +1,15 @@
 import { SequenceType } from '../expressions/dataTypes/Value';
 import astHelper, { IAST } from '../parsing/astHelper';
 
+/**
+ * Checks the type and multiplicity of else clause and then clause
+ * and inserts the type to the ast as an attribute if they match.
+ *
+ * @param ast the AST to be annotated.
+ * @param elseClause the elseClause, uses type data of this param to ast.
+ * @param thenClause the thenClause.
+ * @returns the type of the context item.
+ */
 export function annotateIfThenElseExpr(
 	ast: IAST,
 	elseClause: SequenceType,
