857485890cdb85470f0cb423ae395ed75dc43151
Hector Peeters <h.peeters@student.tudelft.nl>
2021-06-07 10:39:25 +0200

    Disable path annotation in simpleMapExpr
    Since path expressions aren't annotated correctly, and we need those for
    the simpleMapExpr this was causing some problems.

--- src/typeInference/annotateAST.ts
+++ src/typeInference/annotateAST.ts
@@ -321,7 +321,6 @@ const annotationFunctions: {
 	},
 	// Maps
 	simpleMapExpr: (ast: IAST, context: AnnotationContext): SequenceType => {
-		astHelper.getChildren(ast, 'pathExpr').map((c) => annotate(c, context));
 		return annotateSimpleMapExpr(ast, context);
 	},
 	mapConstructor: (ast: IAST, context: AnnotationContext): SequenceType => {
@@ -365,9 +364,6 @@ const annotationFunctions: {
 	},
 	queryBody: (ast: IAST, context: AnnotationContext): SequenceType => {
 		const type = annotate(ast[1] as IAST, context);
-		if (type) {
-			astHelper.insertAttribute(ast, 'type', type);
-		}
 		return type;
 	},
 	flworExpr: (ast: IAST, context: AnnotationContext): SequenceType => {
