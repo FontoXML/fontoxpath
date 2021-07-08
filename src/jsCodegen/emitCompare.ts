feaca22b1e7643ae756c0c0a0128b13ec0de43e2
Palle Maesen <P.maesen@student.tudelftt.nl>
2021-07-08 13:24:51 +0200

    Added template for the generalCompare

--- src/jsCodegen/emitCompare.ts
+++ src/jsCodegen/emitCompare.ts
@@ -1,6 +1,7 @@
-import { IAST } from '../parsing/astHelper';
+import { SequenceMultiplicity, SequenceType } from '../expressions/dataTypes/Value';
+import astHelper, { IAST } from '../parsing/astHelper';
 import { CodeGenContext } from './CodeGenContext';
-import { acceptAst, PartialCompilationResult } from './JavaScriptCompiledXPath';
+import { acceptAst, PartialCompilationResult, rejectAst } from './JavaScriptCompiledXPath';
 
 export function emitValueCompare(
 	ast: IAST,
@@ -10,4 +11,34 @@ export function emitValueCompare(
 	staticContext: CodeGenContext
 ): PartialCompilationResult {
 	return acceptAst('');
+}
+
+export function emitGeneralCompare(
+	ast: IAST,
+	firstExpr: PartialCompilationResult,
+	secondExpr: PartialCompilationResult,
+	identifier: string,
+	staticContext: CodeGenContext
+): PartialCompilationResult {
+	const firstAstOp = astHelper.getFirstChild(ast, 'firstOperand')[1] as IAST;
+	const firstType: SequenceType = astHelper.getAttribute(ast, 'type');
+	const secondAstOp = astHelper.getFirstChild(ast, 'secondOperand')[1] as IAST;
+	const secondType: SequenceType = astHelper.getAttribute(ast, 'type');
+	if (!firstType || !secondType) {
+		return rejectAst('types of compare are not known');
+	}
+	if (
+		firstType.mult === SequenceMultiplicity.EXACTLY_ONE &&
+		secondType.mult === SequenceMultiplicity.EXACTLY_ONE
+	) {
+		return emitValueCompare(ast, firstExpr, secondExpr, identifier, staticContext);
+	} else {
+		// take all the children except for the type.
+		const leftChildren: IAST[] = astHelper.getChildren(firstAstOp, '*');
+		const rightChildren: IAST[] = astHelper.getChildren(secondAstOp, '*');
+		leftChildren.shift();
+		rightChildren.shift();
+		//TODO: implement the generalCompare when the valuecompare is done.
+		return rejectAst('');
+	}
 }
