a912c9f083228b4f833547164fcf999b3476e96a
Palle Maesen <P.maesen@student.tudelftt.nl>
2021-07-19 13:45:27 +0200

    Removed commented code

--- src/jsCodegen/emitCompare.ts
+++ src/jsCodegen/emitCompare.ts
@@ -140,41 +140,6 @@ export function emitGeneralCompare(
 		);
 	} else {
 		return rejectAst('generalCompare with sequences is still in development');
-		// take all the children except for the type.
-		// const leftChildren: IAST[] = astHelper.getChildren(firstAstOp, '*');
-		// const rightChildren: IAST[] = astHelper.getChildren(secondAstOp, '*');
-		// leftChildren.shift();
-		// rightChildren.shift();
-		// // TODO: make the generalCompare compatible with the valueCompare, the code that is here at the moment is a guess
-
-		// let generalCompare: string = `
-		// function ${identifier}(contextItem) {
-		// `;
-
-		// for (let x = 0; x < leftChildren.length; x++) {
-		// 	for (let y = 0; y < rightChildren.length; y++) {
-		// 		const left = emitBaseExpr(leftChildren[x], identifier, staticContext);
-		// 		const right = emitBaseExpr(rightChildren[y], identifier, staticContext);
-		// 		const valueCompare = emitValueCompare(
-		// 			ast,
-		// 			compareType,
-		// 			left,
-		// 			right,
-		// 			identifier,
-		// 			staticContext
-		// 		);
-		// 		if (!valueCompare.isAstAccepted) {
-		// 			return rejectAst('generalCompare could not be created');
-		// 		}
-		// 		// generalCompare += `if(${valueCompare.code}(contextItem)) {
-		// 		//         return true;
-		// 		//     }
-		// 		//     `;
-		// 	}
-		// }
-		// generalCompare += `return false;
-		//                 }`;
-		// return acceptAst(generalCompare);
 	}
 }
 
