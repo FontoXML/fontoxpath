d9556d5d7d3516bfdcf58a2621481ee47a71a448
Jos Verburg <jos.verburg@liones.nl>
2021-09-16 17:24:36 +0200

    Add unit test does not generate compare function for nodes

---
+++ test/specs/expressions/jsCodegen/compare.tests.ts
@@ -0,0 +1,25 @@
+import * as chai from 'chai';
+import { evaluateXPathToBoolean, ReturnType } from 'fontoxpath';
+import * as slimdom from 'slimdom';
+import jsonMlMapper from 'test-helpers/jsonMlMapper';
+import evaluateXPathWithJsCodegen from '../../parsing/jsCodegen/evaluateXPathWithJsCodegen';
+
+describe('compare tests', () => {
+	let documentNode: slimdom.Document;
+	beforeEach(() => {
+		documentNode = new slimdom.Document();
+		jsonMlMapper.parse(['xml', { attr: 'true' }], documentNode);
+	});
+
+	it('does not generate compare function for nodes', () => {
+		const node = documentNode.documentElement;
+		const query = '@attr = true()';
+
+		chai.assert.throws(
+			() => evaluateXPathWithJsCodegen(query, node, null, ReturnType.BOOLEAN),
+			'Unsupported: a base expression used with an operand.'
+		);
+
+		chai.assert.equal(evaluateXPathToBoolean(query, node), true);
+	});
+});
