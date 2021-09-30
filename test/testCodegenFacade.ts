373b20080f5a05eb71b6187a8648fdf77c5dc98d
Marijn Timmerije <M.Timmerije@student.tudelft.nl>
2021-09-30 12:51:52 +0200

    Fixing Codefactor problems.

--- test/testCodegenFacade.ts
+++ test/testCodegenFacade.ts
@@ -3,12 +3,23 @@ import {
 	CDATASection,
 	CharacterData,
 	Comment,
+	compareSpecificity,
 	CompiledXPathFunction,
+	compileXPathToJavaScript,
+	createTypedValueFactory,
 	Document,
+	domFacade,
 	Element,
+	evaluateUpdatingExpression,
+	evaluateUpdatingExpressionSync,
 	EvaluateXPath,
+	evaluateXPath,
+	executeJavaScriptCompiledXPath,
+	executePendingUpdateList,
 	ExternalTypedValueFactory,
 	FunctionNameResolver,
+	getBucketForSelector,
+	getBucketsForNode,
 	IAstAccepted,
 	IAstRejected,
 	IDocumentWriter,
@@ -23,31 +34,20 @@ import {
 	NamespaceResolver,
 	Node,
 	Options,
-	ProcessingInstruction,
-	Profiler,
-	ResolvedQualifiedName,
-	ReturnType,
-	Text,
-	ValidValueSequence,
-	UpdatingOptions,
-	ValidValue,
-	XPathPerformanceMeasurement,
-	compareSpecificity,
-	compileXPathToJavaScript,
-	domFacade,
-	evaluateUpdatingExpression,
-	evaluateUpdatingExpressionSync,
-	evaluateXPath,
-	executeJavaScriptCompiledXPath,
-	executePendingUpdateList,
-	getBucketForSelector,
-	getBucketsForNode,
 	parseScript,
 	precompileXPath,
+	ProcessingInstruction,
+	Profiler,
 	profiler,
 	registerCustomXPathFunction,
 	registerXQueryModule,
-	createTypedValueFactory,
+	ResolvedQualifiedName,
+	ReturnType,
+	Text,
+	UpdatingOptions,
+	ValidValue,
+	ValidValueSequence,
+	XPathPerformanceMeasurement,
 } from '../src/index';
 
 let totalCases = 0;
@@ -60,20 +60,20 @@ const evaluateXPathWithJsCodegen = <
 >(
 	query: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	returnType?: ReturnType,
 	options?: Options,
 	variables?: { [s: string]: any } | null
 ): IReturnTypes<TNode>[TReturnType] => {
-	// console.log(
-	// 	`Total cases: ${totalCases}, JSCodegen cases: ${codegenCases}, Variables cases: ${variablesCases}`
-	// );
 	totalCases++;
 
-	if (variables != {} && variables != null) {
-		//Mark as failing
+	if (variables !== undefined && variables !== null && Object.keys(variables).length !== 0) {
+		// JSCodegen does not support user-defined variables (yet)
 		variablesCases++;
-		return evaluateXPath(query, contextItem, domFacade, variables, returnType, options);
+		// console.log(
+		// 	`Total cases: ${totalCases}, JSCodegen cases: ${codegenCases}, Variables cases: ${variablesCases}`
+		// );
+		return evaluateXPath(query, contextItem, localDomFacade, variables, returnType, options);
 	}
 
 	returnType = returnType || (ReturnType.ANY as any);
@@ -83,24 +83,24 @@ const evaluateXPathWithJsCodegen = <
 		const evalFunction = new Function(compiledXPathResult.code) as CompiledXPathFunction;
 
 		codegenCases++;
-		return executeJavaScriptCompiledXPath(evalFunction, contextItem, domFacade);
+		return executeJavaScriptCompiledXPath(evalFunction, contextItem, localDomFacade);
 	} else {
-		//Mark as failing in csv
-		return evaluateXPath(query, contextItem, domFacade, null, returnType, options);
+		// The query AST was not eligible to be converted to Javascript
+		return evaluateXPath(query, contextItem, localDomFacade, null, returnType, options);
 	}
 };
 
 export function evaluateXPathToArray(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): any[] {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.ARRAY,
 		options,
 		variables
@@ -110,14 +110,14 @@ export function evaluateXPathToArray(
 export function evaluateXPathToAsyncIterator(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): AsyncIterableIterator<any> {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.ASYNC_ITERATOR,
 		options,
 		variables
@@ -127,14 +127,14 @@ export function evaluateXPathToAsyncIterator(
 export function evaluateXPathToBoolean(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): boolean {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.BOOLEAN,
 		options,
 		variables
@@ -144,14 +144,14 @@ export function evaluateXPathToBoolean(
 export function evaluateXPathToFirstNode<T extends Node>(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): T | null {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.FIRST_NODE,
 		options,
 		variables
@@ -161,14 +161,14 @@ export function evaluateXPathToFirstNode<T extends Node>(
 export function evaluateXPathToMap(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): { [s: string]: any } {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.MAP,
 		options,
 		variables
@@ -178,14 +178,14 @@ export function evaluateXPathToMap(
 export function evaluateXPathToNodes<T extends Node>(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): T[] {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.NODES,
 		options,
 		variables
@@ -195,14 +195,14 @@ export function evaluateXPathToNodes<T extends Node>(
 export function evaluateXPathToNumber(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): number {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.NUMBER,
 		options,
 		variables
@@ -212,14 +212,14 @@ export function evaluateXPathToNumber(
 export function evaluateXPathToNumbers(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): number[] {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.NUMBERS,
 		options,
 		variables
@@ -229,14 +229,14 @@ export function evaluateXPathToNumbers(
 export function evaluateXPathToString(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): string {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.STRING,
 		options,
 		variables
@@ -246,14 +246,14 @@ export function evaluateXPathToString(
 export function evaluateXPathToStrings(
 	selector: string,
 	contextItem?: any | null,
-	domFacade?: IDomFacade | null,
+	localDomFacade?: IDomFacade | null,
 	variables?: { [s: string]: any } | null,
 	options?: Options | null
 ): string[] {
 	return evaluateXPathWithJsCodegen(
 		selector,
 		contextItem,
-		domFacade,
+		localDomFacade,
 		ReturnType.STRINGS,
 		options,
 		variables
