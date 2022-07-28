2165fe5f9fc9537d2aa6aa178d549fb7c55a17ad
Jos Verburg <jos.verburg@liones.nl>
2022-07-28 16:53:03 +0200

    Improve trace message

--- src/expressions/functions/builtInFunctions_debugging.ts
+++ src/expressions/functions/builtInFunctions_debugging.ts
@@ -1,7 +1,10 @@
+import { NodePointer } from '../../domClone/Pointer';
+import realizeDom from '../../domClone/realizeDom';
 import atomize from '../dataTypes/atomize';
 import castToType from '../dataTypes/castToType';
+import isSubtypeOf from '../dataTypes/isSubtypeOf';
 import sequenceFactory from '../dataTypes/sequenceFactory';
-import { SequenceMultiplicity, ValueType } from '../dataTypes/Value';
+import { SequenceMultiplicity, ValueType, valueTypeToString } from '../dataTypes/Value';
 import { BUILT_IN_NAMESPACE_URIS } from '../staticallyKnownNamespaces';
 import { BuiltinDeclarationType } from './builtInFunctions';
 import FunctionDefinitionType from './FunctionDefinitionType';
@@ -14,18 +17,23 @@ const fnTrace: FunctionDefinitionType = (
 	label
 ) => {
 	return arg.mapAll((allItems) => {
-		const argumentAsStrings = atomize(sequenceFactory.create(allItems), executionParameters)
-			.map((value) => castToType(value, ValueType.XSSTRING))
-			.getAllValues();
-
 		let newMessage = '';
-		for (let i = 0; i < argumentAsStrings.length; i++) {
-			newMessage +=
-				'{type: ' +
-				argumentAsStrings[i].type +
-				', value: ' +
-				argumentAsStrings[i].value +
-				'}\n';
+		for (let i = 0; i < allItems.length; i++) {
+			const value = allItems[i];
+			const argumentAsString =
+				executionParameters.xmlSerializer && isSubtypeOf(value.type, ValueType.NODE)
+					? executionParameters.xmlSerializer.serializeToString(
+							realizeDom(
+								value.value as NodePointer,
+								executionParameters,
+								false
+							) as Node
+					  )
+					: atomize(sequenceFactory.singleton(value), executionParameters)
+							.map((atomizedValue) => castToType(atomizedValue, ValueType.XSSTRING))
+							.first().value;
+
+			newMessage += `{type: ${valueTypeToString(value.type)}, value: ${argumentAsString}}\n`;
 		}
 		if (label !== undefined) {
 			newMessage += label.first().value;
--- test/specs/parsing/functions/functions.debugging.tests.ts
+++ test/specs/parsing/functions/functions.debugging.tests.ts
@@ -1,25 +1,52 @@
 import * as chai from 'chai';
 import { evaluateXPathToBoolean } from 'fontoxpath';
 import * as sinon from 'sinon';
+import * as slimdom from 'slimdom';
+import jsonMlMapper from 'test-helpers/jsonMlMapper';
 
 describe('debugging functions', () => {
 	describe('fn:trace', () => {
 		let consoleSpy = null;
+		let documentNode: slimdom.Document;
+
 		beforeEach(() => {
+			documentNode = new slimdom.Document();
 			consoleSpy = sinon.spy(console, 'log');
 		});
+
 		afterEach(() => {
 			if (consoleSpy) {
 				consoleSpy.restore();
 				consoleSpy = null;
 			}
 		});
+
 		it('returns the input', () => chai.assert.isTrue(evaluateXPathToBoolean('trace(true())')));
+
 		it('accepts two parameters', () =>
 			chai.assert.isTrue(evaluateXPathToBoolean('trace(true(), "message")')));
+
 		it('outputs the trace', () => {
 			evaluateXPathToBoolean('trace(true())');
 			chai.assert.isTrue(consoleSpy.called);
+		});
+
+		it('serializes nodes', () => {
+			jsonMlMapper.parse(['someElement', 'Some text.'], documentNode);
+			evaluateXPathToBoolean(
+				'trace((., 42, ./someElement/text()))',
+				documentNode,
+				undefined,
+				undefined,
+				{
+					xmlSerializer: new slimdom.XMLSerializer(),
+				}
+			);
+			chai.assert.isTrue(consoleSpy.called);
+			chai.assert.equal(
+				consoleSpy.args[0],
+				'{type: document-node(), value: <someElement>Some text.</someElement>}\n{type: xs:integer, value: 42}\n{type: text(), value: Some text.}\n'
+			);
 		});
 	});
 });
