b20a49b3344ed783317d88855702bdbf81484acc
Martin Middel <martin.middel@fontoxml.com>
2022-10-12 09:05:33 +0200

    Add a unit test showing how circular dependencies should work
    [skip ci]

--- test/specs/parsing/mainModules.tests.ts
+++ test/specs/parsing/mainModules.tests.ts
@@ -123,4 +123,40 @@ declare function fn () external; 1`,
 			'XQST0045'
 		);
 	});
+
+	it.only('Can do circular imports', () => {
+		registerXQueryModule(`
+module namespace test = "http://www.example.org/mainmodules.tests#3";
+
+declare %public function test:AAA($a as xs:integer) as xs:string {
+   if ($a < 0) then "" else "Hello " || test:BBB($a-1)
+};
+`);
+
+		registerXQueryModule(`
+module namespace test = "http://www.example.org/mainmodules.tests#3";
+
+declare %public function test:BBB($a as xs:integer) as xs:string  {
+   if ($a < 0) then "" else test:AAA($a-1) || " World"
+};
+`);
+
+		// finalizeModuleRegistration()
+
+		const result = evaluateXPath(
+			`
+import module namespace test = "http://www.example.org/mainmodules.tests#3";
+
+test:AAA(5)
+`,
+			null,
+			null,
+			null,
+			null,
+			{ language: evaluateXPath.XQUERY_3_1_LANGUAGE }
+		);
+
+		chai.assert.equal(result, 'Hello Hello Hello World World');
+	});
+
 });
